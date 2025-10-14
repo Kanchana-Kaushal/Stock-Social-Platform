18b680a765f6be80b4664414ea6dd4914a4f5e80
Kanchana K. <kanchanak.bandara@gmail.com>
2025-10-14 22:13:33 +0530

    Created User Create (Register) function

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Stock_Social_Platform.Dtos.Account;
+using Stock_Social_Platform.Models;
+
+namespace Stock_Social_Platform.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController(UserManager<AppUser> userManager) : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager = userManager;
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            try
+            {
+                var appUser = new AppUser
+                {
+                    UserName = registerDto.Username,
+                    Email = registerDto.Email,
+                };
+
+                var createUser = await _userManager.CreateAsync(appUser, registerDto.Password);
+
+                if (createUser.Succeeded)
+                {
+                    var roleResult = await _userManager.AddToRoleAsync(appUser, "User");
+                    if (roleResult.Succeeded)
+                    {
+                        return Ok("User Created Successfully");
+                    }else
+                    {
+                        return StatusCode(500, roleResult.Errors);
+                    }
+                }
+                else
+                {
+                    return StatusCode(500, createUser.Errors);
+                }
+                
+            }catch(Exception e)
+            {
+                return StatusCode(500, e);
+            }
+        }
+    }
+}
--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -2,16 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Stock_Social_Platform.Models;
 
 namespace Stock_Social_Platform.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
         public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
         
         public DbSet<Stock> Stock { get; set; }
         public DbSet<Comment> Comment { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new()
+            {
+                new IdentityRole
+                {
+                    Id = "1",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = "2",
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+
     }
 }
